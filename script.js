8146f6305b5c98342e7978d1b27e71a67810e761
Steve Corwin <37982371+PrinceCorwin@users.noreply.github.com>
2021-03-07 14:27:24 -0600

    deleted unnecessary commented out code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // declare variables and event listeners
 let prevNumber = "";
 let currentNumber = "";
-// let operator = "";
 let prevOperator = "";
 let answer;
 let equal = false;
@@ -12,9 +11,6 @@ const numbersClick = document.querySelectorAll(".number");
 const operatorsClick = document.querySelectorAll(".operator");
 
 const allClearClick = document.querySelector(".allClear");
-// allClearClick.ontouchstart = function () {
-//   clear();
-// };
 allClearClick.onclick = function () {
   clear();
 };
@@ -23,42 +19,25 @@ equalClick.onclick = function () {
   equal = true;
   compute();
 };
-// equalClick.ontouchstart = function () {
-//   equal = true;
-//   compute();
-// };
 const deleteClick = document.querySelector(".delete");
 deleteClick.onclick = function () {
   deleteDigit();
 };
-// deleteClick.ontouchstart = function () {
-//   deleteDigit();
-// };
 const signClick = document.querySelector(".changeSign");
 signClick.onclick = function () {
   changeSign();
 };
-// signClick.ontouchstart = function () {
-//   changeSign();
-// };
 
 for (let i = 0; i < numbersClick.length; i++) {
   numbersClick[i].addEventListener("click", function () {
     appendNumber(numbersClick[i].innerText);
   });
-  //   numbersClick[i].addEventListener("ontouchstart", function () {
-  //     appendNumber(numbersClick[i].innerText);
-  //   });
 }
 for (let i = 0; i < operatorsClick.length; i++) {
   operatorsClick[i].addEventListener("click", function () {
     operator = operatorsClick[i].innerText;
     setOperator();
   });
-  //   operatorsClick[i].addEventListener("ontouchstart", function () {
-  //     operator = operatorsClick[i].innerText;
-  //     setOperator();
-  //   });
 }
 
 // functions
